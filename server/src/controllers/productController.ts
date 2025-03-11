7d114970a7ed20b4972db4db7df921b5308e87bb
VolotDmitriy <volotkovskiy.dmitriy@gmail.com>
2025-03-11 16:26:24 +0200

    init: product CRUD

--- server/src/controllers/productController.ts
+++ server/src/controllers/productController.ts
@@ -43,6 +43,7 @@ model ProductVariantAttribute {
   value     String // "256 ГБ"
 }
 
+------------------- READY ---------------------------
 model VariantType {
   id                      String                    @id @default(cuid()) // "varType003"
   categoryId              String // "cat001"
@@ -183,4 +184,135 @@ export const deleteVariantTypeById: RouterHandler = async (req, res) => {
         ErrorHandler(error, res);
     }
 };
-//Need to be tested
+
+/*
+model Product {
+  id          String             @id @default(cuid()) // "prod001"
+  name        String // "Смартфон Samsung Galaxy S24"
+  description String? // "Флагманский смартфон с топовыми характеристиками."
+  categoryId  String // "cat001"
+  category    Category           @relation(fields: [categoryId], references: [id])
+  images      String[] // ["https://example.com/samsung_s24.jpg"]
+  variants    ProductVariant[]
+  attributes  ProductAttribute[]
+  discountId  String? // "disc001"
+  discount    Discount?          @relation(fields: [discountId], references: [id])
+}
+*/
+// PRODUCTS
+export const getAllProducts: RouterHandler = async (req, res) => {
+    try {
+        const products = await prisma.product.findMany();
+
+        res.status(200).json(products);
+    } catch (error) {
+        ErrorHandler(error, res);
+    }
+};
+
+export const getProductById: RouterHandler = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await prisma.product.findUnique({
+            where: { id },
+        });
+
+        if (!product) {
+            res.status(404).json({ message: 'Product not found' });
+            return;
+        }
+
+        res.status(200).json(product);
+    } catch (error) {
+        ErrorHandler(error, res);
+    }
+};
+
+export const getProductByCategory: RouterHandler = async (req, res) => {
+    try {
+        const { categoryId } = req.params;
+        const products = await prisma.product.findMany({
+            where: { categoryId },
+        });
+
+        res.status(200).json(products);
+    } catch (error) {
+        ErrorHandler(error, res);
+    }
+};
+
+export const createProduct: RouterHandler = async (req, res) => {
+    try {
+        const { name, description, categoryId, images, discountId } = req.body;
+
+        const newProduct = await prisma.product.create({
+            data: {
+                name,
+                description: description || undefined,
+                categoryId,
+                images,
+                discountId: discountId || undefined,
+            },
+        });
+
+        res.status(201).json({
+            message: 'Product created successfully',
+            data: newProduct,
+        });
+    } catch (error) {
+        ErrorHandler(error, res);
+    }
+};
+
+export const updateProductById: RouterHandler = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, description, categoryId, images, discountId } = req.body;
+
+        const product = await prisma.product.update({
+            where: { id },
+            data: {
+                name,
+                description: description || undefined,
+                categoryId,
+                images,
+                discountId: discountId || undefined,
+            },
+        });
+
+        if (!product) {
+            res.status(404).json({ message: 'Product not found' });
+            return;
+        }
+
+        res.status(200).json(product);
+    } catch (error) {
+        ErrorHandler(error, res);
+    }
+};
+
+export const deleteProductById: RouterHandler = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await prisma.product.findUnique({
+            where: { id },
+        });
+
+        if (!product) {
+            res.status(404).json({ message: 'Product not found' });
+            return;
+        }
+
+        await prisma.product.delete({
+            where: { id },
+        });
+
+        res.status(200).json({
+            message: 'Product deleted successfully',
+            data: product,
+        });
+    } catch (error) {
+        ErrorHandler(error, res);
+    }
+};
