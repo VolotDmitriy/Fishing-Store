4aff8f901f51929a6f573de6a903ff995c373686
Max Lyubchenko <00eagermax00@gmail.com>
2025-03-16 23:37:54 +0200

    add logger

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,10 @@
 import express, { Application } from 'express';
 import router from './routes';
+import { logger } from './utils/logger';
 
 const app: Application = express();
+
+app.use(logger);
 
 app.use(express.json());
 app.use('/', router);
