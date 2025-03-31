4d67de235e3a7770103b5b656d312c04c923984c
Max Lyubchenko <00eagermax00@gmail.com>
2025-03-31 16:10:29 +0300

    add title to site-header component

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from 'sonner';
 import './globals.css';
 
 export default function RootLayout({
@@ -17,6 +18,7 @@ export default function RootLayout({
                     >
                         {children}
                     </ThemeProvider>
+                    <Toaster />
                 </body>
             </html>
         </>
