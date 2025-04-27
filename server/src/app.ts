ea4381ab5719a1261fe9ba012567d97b47d4f8db
VolotDmitriy <volotkovskiy.dmitriy@gmail.com>
2025-04-28 00:48:51 +0300

    feat: Add JWT auth middleware and normalize line endings in server files

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,23 +1,24 @@
+import cookieParser from 'cookie-parser';
 import cors, { CorsOptions } from 'cors';
 import express, { Application } from 'express';
 import router from './routes';
 import { logger } from './utils/logger';
 
-const app: Application = express();
-
-app.use(logger);
-
 const corsOptions: CorsOptions = {
-    origin: ['http://localhost:3000', 'http://localhost:3001'], // Разрешённый домен
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Разрешённые методы
-    allowedHeaders: ['Content-Type', 'Authorization'], // Разрешённые заголовки
-    credentials: true, // Разрешение куки
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
     optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
+const app: Application = express();
 
+app.use(logger);
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
+
 app.use('/', router);
 
 export default app;
