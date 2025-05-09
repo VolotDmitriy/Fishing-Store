dfa3a0314660a72ad796e52cee11ee51a36ce7eb
VolotDmitriy <volotkovskiy.dmitriy@gmail.com>
2025-05-09 23:55:20 +0300

    add: cart table-summary component

---
+++ client/src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Cart from '@/components/cart-table-summary';
+
+export default function CartPage() {
+    return (
+        <div className=" bg-[#141414]">
+            <Cart />
+        </div>
+    );
+}
