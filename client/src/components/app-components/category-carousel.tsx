61d6860749d2931291ec381d58a21827200ddcb1
Daniil <prowerdan@gmail.com>
2025-05-23 22:46:35 +0300

    update category-carousel: add fetchCategories and skeleton loading

--- client/src/components/app-components/category-carousel.tsx
+++ client/src/components/app-components/category-carousel.tsx
@@ -1,8 +1,46 @@
-import React from 'react';
+'use client';
+
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import CategoryCard from '../category-card';
 import UniversalCarousel from '../universal-carousel';
 
+interface CategoryTypeF {
+    id: string;
+    name: string;
+    parentId: string | null;
+    createdAt: string;
+    updatedAt: string;
+}
+
 const CategoryCarousel: React.FC = () => {
+    const [categories, setCategories] = useState<CategoryTypeF[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true); 
+
+    const images = [
+        'Categories/Reels.png',
+        'Categories/Hooks.jpg',
+        'Categories/Accessories.jpg',
+    ];
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const res = await axios.get('http://localhost:4200/category');
+                if (res.status === 200) {
+                    setCategories(res.data);
+                } else {
+                    throw new Error('Failed to fetch categories');
+                }
+            } catch (error) {
+                console.error('Error fetching categories:', error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchCategories();
+    }, []);
+
     return (
         <div className="w-full flex justify-center bg-[#141414]">
             <div
@@ -17,32 +55,23 @@ const CategoryCarousel: React.FC = () => {
                 >
                     SHOP BY CATEGORY
                 </h2>
-
                 <UniversalCarousel itemsPerView={3}>
-                    <CategoryCard
-                        title="Катушки"
-                        imageSrc="Categories/Reels.png"
-                    />
-                    <CategoryCard
-                        title="Крючки"
-                        imageSrc="Categories/Hooks.jpg"
-                    />
-                    <CategoryCard
-                        title="Аксессуары"
-                        imageSrc="Categories/Accessories.jpg"
-                    />
-                    <CategoryCard
-                        title="Катушки"
-                        imageSrc="Categories/Reels.png"
-                    />
-                    <CategoryCard
-                        title="Крючки"
-                        imageSrc="Categories/Hooks.jpg"
-                    />
-                    <CategoryCard
-                        title="Аксессуары"
-                        imageSrc="Categories/Accessories.jpg"
-                    />
+                    {isLoading
+                        ? Array.from({ length: 3 }).map((_, index) => (
+                              <div
+                                  key={index}
+                                  className="w-full flex justify-center mx-4 animate-pulse"
+                              >
+                                  <div className="w-full max-w-[500px] aspect-square bg-gray-500 rounded-md"></div>
+                              </div>
+                          ))
+                        : categories.map((category, index) => (
+                              <CategoryCard
+                                  key={category.id}
+                                  title={category.name}
+                                  imageSrc={images[index % images.length]}
+                              />
+                          ))}
                 </UniversalCarousel>
             </div>
         </div>
