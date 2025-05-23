f6810c1f153e0d70fb78f37c9d0f39a50ba22d76
Daniil <prowerdan@gmail.com>
2025-05-23 22:51:27 +0300

    update Header: micto-fix

--- client/src/components/app-components/header.tsx
+++ client/src/components/app-components/header.tsx
@@ -2,13 +2,12 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import Cart from './public/Cart icon.png';
-import Favorite from './public/Favorite icon.png';
 import Logo from './public/game-icons_fishing-net.png';
 import SearchBar from './searchBar';
 
 const Header: React.FC = () => {
     return (
-        <header className="top-0 left-0 w-full h-20 bg-[#141414] border-b border-[#E5E8EB] z-50 box-border mb-[30px]">
+        <header className="top-0 left-0 w-full h-20 bg-[#141414] border-b border-[#E5E8EB] z-50 box-border mb-[30px] sticky">
             <div className="h-full  mx-auto flex items-center justify-between">
                 <div className="flex shrink-0 pl-[40px] py-[10px]">
                     <Link href="/" className="flex flex-row items-center gap-2">
@@ -27,7 +26,7 @@ const Header: React.FC = () => {
                         Home
                     </Link>
                     <Link
-                        href="/categories"
+                        href="/catalog"
                         className="text-white font-plus-jakarta-sans font-medium text-[20px] leading-[21px] whitespace-nowrap hover:text-[#38E078]"
                     >
                         Categories
@@ -46,16 +45,6 @@ const Header: React.FC = () => {
 
                 <div className="flex justify-end px-[36px]">
                     <div className="flex flex-row justify-center items-center gap-[8px] ">
-                        <button className="w-[40px] h-[40px] bg-[#292929] rounded-[20px] flex justify-center items-center">
-                            <Link href="/favorites" className="">
-                                <Image
-                                    src={Favorite}
-                                    alt="Favorite"
-                                    width={20}
-                                    height={16}
-                                />
-                            </Link>
-                        </button>
                         <button className="w-[40px] h-[40px] bg-[#292929] rounded-[20px] flex justify-center items-center">
                             <Link href="/cart" className="">
                                 <Image
