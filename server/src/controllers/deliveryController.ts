7ba96064d8955f7c7985bb3c22d95ace712e8f9b
VolotDmitriy <volotkovskiy.dmitriy@gmail.com>
2025-05-25 17:10:46 +0300

    add: add server cache

--- server/src/controllers/deliveryController.ts
+++ server/src/controllers/deliveryController.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import NodeCache from 'node-cache';
 import { RouterHandler } from '../utils/controllerUtils';
 
+const cache = new NodeCache({ stdTTL: 600 }); // Кеш на 10 хвилин
 const apiUrl = process.env.NOVA_POSHTA_API_URL;
 const apiKey = process.env.NOVA_POSHTA_API_KEY;
 
@@ -8,10 +10,8 @@ const warehouseRefs = {
     warehouse: [
         '6f8c7162-4b72-4b0a-88e5-906948c6a92f', // Поштове відділення з обмеження
         '841339c7-591a-42e2-8233-7a0a00f0ed6f', // Поштове(ий)
-        '9a68df70-0267-42a8-bb5c-37f427e36ee4', // Вантажне(ий)
     ],
     postomat: [
-        '95dc212d-479c-4ffb-a8ab-8c1b9073d0bc', // Поштомат ПриватБанку
         'f9316480-5f2d-425d-bc2c-ac7cd29decf0', // Поштомат
     ],
 };
@@ -64,14 +64,22 @@ export const getWarehouses: RouterHandler = async (req, res) => {
 
     if (!settlementRef || !warehouseType) {
         res.status(400).json({
-            error: "settlementRef та warehouseType обов'язкові",
+            error: 'settlementRef и warehouseType обязательны',
         });
+        return;
+    }
+
+    const cacheKey = `${settlementRef}_${warehouseType}`;
+    const cachedData = cache.get(cacheKey);
+
+    if (cachedData) {
+        res.json(cachedData);
         return;
     }
 
     if (!apiUrl || !apiKey) {
         res.status(500).json({
-            error: 'Помилка конфігурації: відсутній API URL або ключ',
+            error: 'Ошибка конфигурации: отсутствует API URL или ключ',
         });
         return;
     }
@@ -94,13 +102,29 @@ export const getWarehouses: RouterHandler = async (req, res) => {
         const allWarehouses = responses.flatMap((response: any) => {
             if (response.data.success) {
                 return response.data.data;
+            } else {
+                console.error(
+                    `Ошибка API для TypeOfWarehouseRef ${response.config.data}:`,
+                    response.data.errors,
+                );
+                return [];
             }
-            return [];
         });
 
+        if (allWarehouses.length === 0) {
+            res.status(404).json({
+                error: 'Склады не найдены',
+                details: responses
+                    .map((r: any) => r.data.errors)
+                    .filter(Boolean),
+            });
+            return;
+        }
+
+        cache.set(cacheKey, allWarehouses);
         res.json(allWarehouses);
     } catch (error) {
-        console.error('Помилка при запиті до Нова Пошта:', error);
-        res.status(500).json({ error: 'Помилка сервера' });
+        console.error('Ошибка при запросе к Нова Пошта:', error);
+        res.status(500).json({ error: 'Ошибка сервера' });
     }
 };
