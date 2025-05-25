4978db2450c7debb4f436ba7ea367bf76e5d5fd5
Daniil <prowerdan@gmail.com>
2025-05-25 19:13:10 +0300

    product-carousel: implement functional

--- client/src/components/app-components/product-carousel.tsx
+++ client/src/components/app-components/product-carousel.tsx
@@ -1,84 +1,51 @@
-import React from 'react';
+'use client';
+
+import { ProductType } from '@/components/data-table/types';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import ProductCard from '../product-card';
 import UniversalCarousel from '../universal-carousel';
 
-interface Product {
-    id: string;
-    name: string;
-    imageSrc: string;
-    price: number;
-    sizes: string[];
-    weights: string[];
-}
-
-const products: Product[] = [
-    {
-        id: 'prod001',
-        name: 'Бойли розчинні SquidBerry',
-        imageSrc: 'Products/1.jpg',
-        price: 405,
-        sizes: ['20 мм', '24 мм'],
-        weights: ['zip-уп 1 кг', 'відро 3 кг'],
-    },
-    {
-        id: 'prod002',
-        name: 'Бойли Krill Fusion',
-        imageSrc: 'Products/2.jpg',
-        price: 379,
-        sizes: ['16 мм', '20 мм'],
-        weights: ['zip-уп 0.5 кг', 'відро 2 кг'],
-    },
-    {
-        id: 'prod003',
-        name: 'Pop-ups Ananas',
-        imageSrc: 'Products/3.jpg',
-        price: 189,
-        sizes: ['10 мм', '14 мм'],
-        weights: ['банка 100г'],
-    },
-    {
-        id: 'prod004',
-        name: 'Крючки RB-28419',
-        imageSrc: 'Products/4.jpg',
-        price: 129,
-        sizes: ['M', 'L'],
-        weights: ['упаковка 10 шт'],
-    },
-    {
-        id: 'prod005',
-        name: 'Бойли розчинні SquidBerry',
-        imageSrc: 'Products/1.jpg',
-        price: 405,
-        sizes: ['20 мм', '24 мм'],
-        weights: ['zip-уп 1 кг', 'відро 3 кг'],
-    },
-    {
-        id: 'prod006',
-        name: 'Бойли Krill Fusion',
-        imageSrc: 'Products/2.jpg',
-        price: 379,
-        sizes: ['16 мм', '20 мм'],
-        weights: ['zip-уп 0.5 кг', 'відро 2 кг'],
-    },
-    {
-        id: 'prod007',
-        name: 'Pop-ups Ananas',
-        imageSrc: 'Products/3.jpg',
-        price: 189,
-        sizes: ['10 мм', '14 мм'],
-        weights: ['банка 100г'],
-    },
-    {
-        id: 'prod008',
-        name: 'Крючки RB-28419',
-        imageSrc: 'Products/4.jpg',
-        price: 129,
-        sizes: ['M', 'L'],
-        weights: ['упаковка 10 шт'],
-    },
-];
-
 const ProductCarousel: React.FC = () => {
+    const [products, setProducts] = useState<ProductType[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const res = await axios.get<ProductType[]>(
+                    'http://localhost:4200/product?full=true',
+                );
+                if (res.status === 200) {
+                    const sortedProducts = res.data.sort(
+                        (a, b) =>
+                            new Date(b.createdAt).getTime() -
+                            new Date(a.createdAt).getTime(),
+                    );
+                    const limitedProducts = sortedProducts.slice(0, 10);
+                    setProducts(limitedProducts);
+                } else {
+                    throw new Error('Failed to fetch products');
+                }
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchProducts();
+    }, []);
+
+    const renderSkeleton = () =>
+        Array.from({ length: 4 }).map((_, index) => (
+            <div
+                key={index}
+                className="w-full h-full flex justify-center mx-6 animate-pulse"
+            >
+                <div className="w-full h-[532px] bg-gray-500 rounded-md"></div>
+            </div>
+        ));
+
     return (
         <div className="w-full flex justify-center bg-[#242424]">
             <div
@@ -91,21 +58,14 @@ const ProductCarousel: React.FC = () => {
                         'font-brigend text-[44px] leading-[1] font-bold pt-[82px]'
                     }
                 >
-                    TOP PRODUCTS
+                    NEW ARRIVALS
                 </h2>
-
                 <UniversalCarousel itemsPerView={4}>
-                    {products.map((product) => (
-                        <ProductCard
-                            key={product.id}
-                            id={product.id}
-                            imageSrc={product.imageSrc}
-                            title={product.name}
-                            price={product.price}
-                            sizes={product.sizes}
-                            weights={product.weights}
-                        />
-                    ))}
+                    {isLoading
+                        ? renderSkeleton()
+                        : products.map((product) => (
+                              <ProductCard key={product.id} item={product} />
+                          ))}
                 </UniversalCarousel>
             </div>
         </div>
