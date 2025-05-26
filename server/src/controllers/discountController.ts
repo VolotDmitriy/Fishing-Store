faa4169f82bd6e7bf9177b43b3c38668266bfb18
VolotDmitriy <volotkovskiy.dmitriy@gmail.com>
2025-05-26 14:05:19 +0300

    add: processing discount check

--- server/src/controllers/discountController.ts
+++ server/src/controllers/discountController.ts
@@ -143,3 +143,34 @@ export const deleteDiscount: RouterHandler = async (req, res) => {
         ErrorHandler(error, res);
     }
 };
+
+export const checkDiscount: RouterHandler = async (req, res) => {
+    try {
+        const { code } = req.body;
+        const now = new Date();
+        const discount = await prisma.discount.findFirst({
+            where: {
+                name: code,
+                startDate: { lte: now },
+                endDate: { gte: now },
+            },
+        });
+        if (!discount) {
+            res.status(200).json({
+                message: 'Discount not found or not active',
+            });
+            return;
+        }
+        res.status(200).json({
+            message: 'Coupon applied successfully',
+            discount: {
+                id: discount.id,
+                name: discount.name,
+                percentage: discount.percentage,
+            },
+        });
+        return;
+    } catch (error) {
+        ErrorHandler(error, res);
+    }
+};
